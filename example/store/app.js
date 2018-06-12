fa03570619b47470bed94bb87b59faed41a9ee34
mayoo <maythinzaroo@amdon.com>
2018-06-12 16:55:19 +0630

    uploaded tutorial files

---
+++ example/store/app.js
@@ -0,0 +1,15 @@
+(function(){
+	var app = angular.module('store',[ ]);
+
+	app.controller('StoreController', function(){
+		this.product= gem;
+	});
+
+	var gem = {
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gem.',
+		canPurchase: true,
+		soldOut: true,
+	}
+})();
